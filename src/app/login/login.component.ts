ba012e2789692fde5b0bfbf27d68a606715d5ad1
Michael <michaelhuytran@gmail.com>
2016-10-13 12:48:27 -0700

    [feature] - google documentation doesn't work; not building out modal, and functions from scratch

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { UserService } from "./../user.service";
-// import gapi from "gapi";
-import $ from "jquery";
 
 @Component({
   selector: 'login',
@@ -34,23 +32,10 @@ export class LoginComponent {
     );
   }
 
-  signInCallback(authResult) {
-    if (authResult['code']) {
-      $('#signinButton').attr('style', 'display: none');
-      // Send the code to the server
-      $.ajax({
-        type: 'POST',
-        url: 'http://localhost:4200/api/admin/googleLogin',
-        contentType: 'application/octet-stream; charset=utf-8',
-        success: function(result) {
-          console.log(result, "SUCCESSFUL POST TO BACKEND")
-        },
-        processData: false,
-        data: authResult['code']
-      });
-    } else {
-      console.log("THERE WAS AN ERROR IN SIGNINCALLBACK");
-    }
+  signInCallback() {
+    var location = global.window.location;
+    var port = (location.port) ? ':' + location.port : '';
+    this.userService.googleLogin(location.protocol + '//' + location.hostname + port);
   }
   //
   // signOut() {
